a814fcb5f9a7927855f10bbe6474aebf06d104fd
Tatymusya <tatyamusya@yandex.ru>
2021-11-03 23:19:22 +0300

    Добавляю ф-ционал 3 урока

---
+++ src/assets/vendors/D3/index.js
@@ -0,0 +1,11 @@
+import { select, selectAll, append, attr, classed, html, on } from 'd3-selection';
+
+export default {
+    select,
+    selectAll,
+    append,
+    attr,
+    classed,
+    html,
+    on,
+};
